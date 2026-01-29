797d38e95a5021019739bab6dc6b36e8432e81b6
danadorn <dorndana8899@gmail.com>
2026-01-29 11:28:02 +0700

    add skeleton loading to user card page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { UserCard } from "@/component/cards/UserCard";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export default function page() {
   return (
     <>
     <UserCard />
+    <Skeleton />
     </>
   );
 }
---
+++ app/products/loading.tsx
@@ -0,0 +1,12 @@
+import { SkeletonCard } from '@/component/skeletons/skeleton-card'
+import React from 'react'
+
+export default function ProductLoading() {
+  return (
+    <>
+    <main className='container mx-auto p-4 grid grid-cols-4 gap-6'>
+    {Object.keys(new Array(20).fill(0)).map((key) => <SkeletonCard key={key} />)}
+    </main>
+    </>
+  )
+}
